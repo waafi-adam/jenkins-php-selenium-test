4630a30a963ef0d864087308a47640ba10b04c72
Waafi Adam <80448912+waafi-adam@users.noreply.github.com>
2024-07-22 09:06:27 +0800

    update deploy

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\waafi\\OneDrive\\Desktop\\ssd\\lab07\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/07b-jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
-
